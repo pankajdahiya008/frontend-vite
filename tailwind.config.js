dd1600859baff4412bfae3e562b9a51e8f4e8695
Pankaj Dahiya <pankaj.id.008@gmail.com>
2026-01-10 15:49:22 +0530

    added port and primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#1e40af', // dark blue
+      },
+    },
   },
   plugins: [],
 }
